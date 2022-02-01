bd19d456409a89f8eba90d4614bf73a85484a935
swathi-22 <mkswathisuresh@gmail.com>
2022-02-01 17:07:31 +0530

    Gadlix_Dynamic_basic

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def first(request):
+
+    print('#'*50)
+    print(request)
+
+    return render(request,'web/index.html')
