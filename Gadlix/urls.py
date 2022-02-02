7c57c2abb716c49b441c6da20f812a06a8b284ab
swathi-22 <mkswathisuresh@gmail.com>
2022-02-02 15:38:07 +0530

    GAdlix-Dynamic

--- Gadlix/urls.py
+++ Gadlix/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
-from home import models
-from home import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls')),
+    path('',include('Web.urls',namespace="Web")),
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Web/views.py
+++ Web/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Service, Term
 # Create your views here.
+
+
+def index(request):
+    context={
+
+    }
+    return render(request,'web/index.html',context)
+
+
+def about(request):
+    context={
+
+    }
+    return render(request,'web/about.html',context)
+
+
+def services(request):
+    services = Service.objects.all()
+    context={
+        "services":services,
+
+    }
+    return render(request,'web/services.html',context)
+
+
+def serviceDetails(request,id):
+    service=Service.objects.get(id=id)
+    terms=Term.objects.filter(service=service)
+    context={
+        "service":service,
+        "terms":terms
+    }
+    return render(request,'web/service-details.html',context)
+
+
+def gallery(request):
+    context={
+
+    }
+    return render(request,'web/gallery.html',context)
+
+
+def blog(request):
+    context={
+
+    }
+    return render(request,'web/blog.html',context)
+
+
+def blogDetails(request):
+    context={
+
+    }
+    return render(request,'web/blog-details.html',context)
+
+
+def contact(request):
+    context={
+
+    }
+    return render(request,'web/contact.html',context)
